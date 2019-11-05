b4bdc09018038d93bfa08547c7f58cc16cd1eeaa
Reelix <reelix+github@gmail.com>
2019-11-05 21:29:10 +0200

    Multi-user support

--- reepwn.sh
+++ reepwn.sh
@@ -4,10 +4,15 @@ CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
 # Code
+theUser='root'
+if [ -n "$1" ]
+then
+   theUser=$1
+fi
 echo ""
 echo ""
 echo -e "${NC}+------------------------------------------------------------------+"
-echo -e "|${YELLOW}ReePwn Version 0.2 - By: Reelix - https://github.com/Reelix/reepwn${NC}|"
+echo -e "|${YELLOW}ReePwn Version 0.3 - By: Reelix - https://github.com/Reelix/reepwn${NC}|"
 echo -e "+------------------------------------------------------------------+"
 echo ""
 echo -e "${YELLOW}OS${NC}"
@@ -18,9 +23,9 @@ echo -e "${YELLOW}Bash history${NC}"
 echo -e "------------${CYAN}"
 cat ~/.bash_history
 echo ""
-echo -e "${YELLOW}SUID things you can run owned by root (https://gtfobins.github.io/)${NC}"
+echo -e "${YELLOW}SUID things you can run owned by ${theUser} (https://gtfobins.github.io/)${NC}"
 echo -e "-------------------------------------------------------------------${CYAN}"
-find / -user root -group $USER -perm /4000 2>/dev/null | xargs ls -l
+find / -user ${theUser} -group $USER -perm /4000 2>/dev/null
 echo ""
 echo -e "${YELLOW}Useful /etc/passwd people (uid 0, 500, 501, 502, 1000, 1001, 1002, 2000, 2001, 2002)${NC}"
 echo -e "------------------------------------------------------------------------------------${CYAN}"
@@ -40,19 +45,24 @@ echo -e "---------------------------------------${CYAN}"
 # /usr/bin/journalctl | grep root | sort
 which journalctl 2>/dev/null
 echo ""
-echo -e "${YELLOW}Finding root owned directories that you can write to${NC}"
+echo -e "${YELLOW}Finding ${theUser} owned directories that you can write to${NC}"
 echo -e "----------------------------------------------------${CYAN}"
-find / -user root -type d -perm -722 2>/dev/null
+find / -user ${theUser} -type d -perm -722 2>/dev/null
 echo ""
-echo -e "${YELLOW}Finding root owned directories that your group can access${NC}"
+echo -e "${YELLOW}Finding ${theUser} owned directories that your group can access${NC}"
 echo -e "---------------------------------------------------------${CYAN}"
-find / -user root -type d -group $(id -g -n) 2>/dev/null
+find / -user ${theUser} -type d -group $(id -g -n) 2>/dev/null
 echo ""
-echo -e "${YELLOW}Finding root owned files that you can edit${NC}"
+echo -e "${YELLOW}Finding ${theUser} owned files that you can edit${NC}"
 echo -e "------------------------------------------${CYAN}"
-find / -user root -type f -perm -722 2>/dev/null
+find / -user ${theUser} -type f -perm -722 2>/dev/null
 echo ""
+if [ ${theUser} != 'root' ]
+then
+echo -e "${YELLOW}Finding ${theUser} owned files that you can view${NC}"
+echo -e "------------------------------------------${CYAN}"
+find / -user ${theUser} -type f -readable 2>/dev/null
+echo ""
+fi
 echo -e "${YELLOW}The End${NC}"
-# export PS1="\e[1;93m\u@\h \w> \e[m"
-echo "Reelix's Terminal Fix: export PS1=\"\e[1;93m\u@\h \w> \e[m\""
 echo "Just something else: cat /proc/sys/kernel/randomize_va_space (0=off, 2=?)"
